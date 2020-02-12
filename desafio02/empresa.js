f6b113dda5909a770f62a00eedb75dce66df10bc
Altamir Santos <altamir.santos@g.globo>
2020-02-12 12:30:08 -0300

    Object

---
+++ desafio02/empresa.js
@@ -0,0 +1,13 @@
+// Construction and printing of objects
+
+const company = {
+    name: "Rocketseat",
+    color: "Roxo",
+    focus: "Programação",
+    address:{
+        street:"Rua Guilherme Gembala",
+        number:260,
+    },
+}
+
+console.log(`A empresa ${company.name} está localizada na ${company.address.street}, ${company.address.number}`)
